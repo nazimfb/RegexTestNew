8c7c4b6ed45410d25991e6f9a19b2a53c0853a6f
nazimfb <nazimbudaqli@gmail.com>
2025-10-28 15:06:21 +0400

    initial commit

--- src/main/java/az/budaqli/Main.java
+++ src/main/java/az/budaqli/Main.java
@@ -1,17 +1,12 @@
 package az.budaqli;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Pattern pattern = Pattern.compile("^[+]{0,1}994(10|50|51|55|70|77|99)\\d{7}$");
+        Matcher matcher = pattern.matcher("+994556429058");
+        System.out.println(matcher.matches());
     }
 }

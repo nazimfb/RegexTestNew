7dcf0741680a775c550aa5ee48eaa607d08fec62
nazimfb <nazimbudaqli@gmail.com>
2025-10-30 21:54:20 +0400

    added email regex

--- src/main/java/az/budaqli/Main.java
+++ src/main/java/az/budaqli/Main.java
@@ -5,8 +5,14 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("^[+]{0,1}994(10|50|51|55|70|77|99)\\d{7}$");
-        Matcher matcher = pattern.matcher("+994556429058");
+        //so its the number format
+        Pattern number = Pattern.compile("^[+]?994(10|50|51|55|70|77|99)\\d{7}$");
+        Matcher matcher = number.matcher("+994556429058");
         System.out.println(matcher.matches());
+        // and this is the email pattern-
+        Pattern email = Pattern.compile("^[\\w-.]{1,49}@([\\w-]+\\.){2,5}.[a-zA-Z]{2,4}$");
+        matcher = email.matcher("nazim-.budaqlIII@-out-look.rtr.turk");
+        System.out.println(matcher.matches());
+
     }
 }
